788d929e85c4fecdae84cf987b1e77d27fd02f87
buzzy-coder <tanisha985327@gmail.com>
2025-07-03 23:11:00 +0530

    new changes to main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 mod hashing;
 mod scanner;
 
+use std::env;
 use std::path::PathBuf;
 use hashing::HashAlgorithm;
 use scanner::{scan_directory, process_files_parallel};
 
 fn main() {
-    let target_dir = PathBuf::from("C:/Users/honey/OneDrive/Documents/file-testing for deduplicator");
-    let files = scan_directory(&target_dir);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <folder_path>", args[0]);
+        return;
+    }
 
-    println!("Found {} files. Processing...", files.len());
+    let folder_path = PathBuf::from(&args[1]);
+    let files = scan_directory(&folder_path);
     process_files_parallel(&files, HashAlgorithm::Sha256);
 }
 
